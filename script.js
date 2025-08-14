b146a7c0fd102d4167cfa5bd4c053ec0e43d580f
rishithapaladugu <116055808+rishithapaladugu@users.noreply.github.com>
2025-08-14 12:02:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,6 +4,19 @@ document.body.appendChild(getSumBtn);
 
 const getSum = () => {
 //Add your code here
+	let sum=0;
+	for(let i=0;i<prices.length;i++){
+	let price=parseInt(prices[i].textContent);
+	sum=sum+price;
+}
+	let tr=table.insertRow();
+	let td1=tr.insertCell(0);
+	let td2=tr.insertCell(1);
+	td1.textContent="total"
+	td2.textContent=sum;
+	td2.id="ans"
+	
+	
   
 };
 
